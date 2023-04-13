11dce70c6c00cd8341d21057c7ba37e6e5b8e28b
Kzdevelop25 <tlod9906@gmail.com>
2023-04-13 16:10:32 +0800

    add new database user

--- conn.php
+++ conn.php
@@ -1,9 +1,9 @@
 <?php
 
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$db_name = "todolist_latihan";
+$hostname = "u8bf0tmf";
+$username = "u8bf0tmf_todo";
+$password = "s)W*=?b]X*yt";
+$db_name = "u8bf0tmf_todo";
 
 $conn = mysqli_connect($hostname,$username,$password,$db_name) OR die();
 
